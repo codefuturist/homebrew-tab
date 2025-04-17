aab20ff3ca273053d44deea3dc07eb3cccd16e3b
Kithrian <58808821+codefuturist@users.noreply.github.com>
2025-04-17 13:36:36 +0200

    -

--- Formula/pwgn.rb
+++ Formula/pwgn.rb
@@ -10,8 +10,10 @@ class Pwgn < Formula
   # url "https://github.com/codefuturist/monorepo-public/archive/refs/tags/pwgn-0.0.1.tar.gz"
   # url "https://github.com/rclone/rclone/archive/refs/tags/v1.69.1.tar.gz"
   # sha256 "46015ab6d2b1794767aae57e39e5fa79a4dccf6f7c3e855ec1572a363272dc91"
-  sha256 "5e8349c9bbaec8dbd2b48f61287f804362b4ffe5556b22bb0b5f78e613bdc0b0"
+  # sha256 "5e8349c9bbaec8dbd2b48f61287f804362b4ffe5556b22bb0b5f78e613bdc0b0"
   # sha256 "9bda1fb6a11a317be41719fb99c455c78e4c83967f08c5b263a4bd074aaf8e9f"
+  sha256 "3739b850316531b5265589c8d2411bdb10d77674f3a4863278f1cd8be00e04df"
+  # sha256 "21ebfe7bf04eca6d01b0a5e3dbd2423525dc0c78c7fb6c01a5ae84d62db225c3"
   version "0.0.1"
   license ""
 
